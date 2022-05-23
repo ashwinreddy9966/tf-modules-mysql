dc42c2332e83fe7003eb0ffea60aa8454499545a
manu <manu@local.com>
2022-05-23 23:07:53 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,33 +1,3 @@
-
-
-
-
-
-
-
-
-#resource "aws_elasticache_cluster" "redis" {
-#  cluster_id           = "roboshop-${var.ENV}"
-#  engine               = "redis"
-#  node_type            = "cache.t3.small"
-#  num_cache_nodes      = 1
-#  parameter_group_name = aws_elasticache_parameter_group.default.name
-#  engine_version       = "6.x"
-#  port                 = 6379
-#  subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
-#  security_group_ids  = [aws_security_group.mysql.id]
-#}
-#
-#resource "aws_elasticache_parameter_group" "default" {
-#  name   = "roboshop-redis-${var.ENV}"
-#  family = "redis6.x"
-#}
-#
-#resource "aws_elasticache_subnet_group" "subnet-group" {
-#  name       = "roboshop-${var.ENV}"
-#  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-#}
-
 resource "aws_security_group" "mysql" {
   name        = "roboshop-mysql-${var.ENV}"
   description = "roboshop-mysql-${var.ENV}"
@@ -60,3 +30,33 @@ resource "aws_security_group" "mysql" {
     Name =  "roboshop-redis-sg-${var.ENV}"
   }
 }
+
+
+
+
+
+
+
+
+#resource "aws_elasticache_cluster" "redis" {
+#  cluster_id           = "roboshop-${var.ENV}"
+#  engine               = "redis"
+#  node_type            = "cache.t3.small"
+#  num_cache_nodes      = 1
+#  parameter_group_name = aws_elasticache_parameter_group.default.name
+#  engine_version       = "6.x"
+#  port                 = 6379
+#  subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
+#  security_group_ids  = [aws_security_group.mysql.id]
+#}
+#
+#resource "aws_elasticache_parameter_group" "default" {
+#  name   = "roboshop-redis-${var.ENV}"
+#  family = "redis6.x"
+#}
+#
+#resource "aws_elasticache_subnet_group" "subnet-group" {
+#  name       = "roboshop-${var.ENV}"
+#  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+#}
+
