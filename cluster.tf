8e4045c29a6aed6e0c89ac2e5019610301de2e19
manu <manu@local.com>
2022-05-23 23:29:33 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -38,13 +38,13 @@ resource "aws_security_group" "mysql" {
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
-  ingress {
-    description = "Allows Def Subnet CIDR"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
-  }
+#  ingress {
+#    description = "Allows Def Subnet CIDR"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+#  }
 
   egress {
     from_port        = 0
