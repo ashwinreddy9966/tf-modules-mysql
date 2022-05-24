6f92d50604dc645c56c03efc69b2a31e0f04ccb6
manu <manu@local.com>
2022-05-24 10:57:26 +0530

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "ENV" {}
 variable "WORKSTATION_IP" {}
 
 #RDS
-variable "var.RDS_MYSQL_PORT" {}
+variable "RDS_MYSQL_PORT" {}
 variable "RDS_MYSQL_STORAGE" {}
 variable "RDS_ENGINE_VERSION" {}
 variable "RDS_INSTANCE_TYPE" {}
